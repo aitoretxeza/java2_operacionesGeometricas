9eb0ce6440ad88d0500c617836d4e79c6206131d
aitoretxeza <aitoretxeza@gmail.com>
2014-10-09 09:18:17 +0200

    OperacionesGeometricas

--- src/OperacionesGeometricas.java
+++ src/OperacionesGeometricas.java
@@ -4,91 +4,47 @@ import com.zubiri.geometria.Circulo;
 
 import com.zubiri.geometria.Rectangulo;
 
-
-
-/*
-
- *Programa para realizar operaciones geometricas.
-
- */
+//Programa para realizar operaciones geometricas.
 
 public class OperacionesGeometricas {
 
-
-
 	public static void main (String args[]) {		
-
-
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("\nIngresa la figura geometrica (Circulo o Rectangulo): ");
+		System.out.print("\nIngresa la figura geometrica Circulo (C) o Rectangulo (R) o Terminar el programa (T): ");
 
+		String respuesta = sc.nextLine();
+ 
+		char respuestaChar = respuesta.charAt(0);
 		
+		while ((respuestaChar != 'T') && (respuestaChar != 't')){
 
-		String str1 = sc.nextLine();
-
-		String str2 = "Circulo";
-
-
-
-		int result = str1.compareToIgnoreCase( str2 );
-
-			if (result == 0){
-
-				
+			if ((respuestaChar == 'C') || (respuestaChar == 'c')){
 
 				double radioIntroducido, area, circunferencia;
 
-
-
-				Circulo circulo1 = new Circulo();
-
-	
-
 				Scanner sc1 = new Scanner(System.in);
-
-
 
 				System.out.print("\nIntroduce el radio del circulo: ");
 
-
-
 				radioIntroducido = sc1.nextDouble();
 
-
-
-				circulo1.setRadio(radioIntroducido);
-
-
+				Circulo circulo1 = new Circulo(radioIntroducido);
 
 				area = circulo1.area();
 
 				circunferencia = circulo1.circunferencia();
 
-
-
 				System.out.println("\nEl area del circulo es: " + area + "m2");
 
 				System.out.println("\nLa circunferencia del circulo es: " + circunferencia + "m");
-
-				
-
+			
 			}
 
-			else{
+			else if ((respuestaChar == 'R') || (respuestaChar == 'r')){
 
-				
-
-				
-
-				double altoIntroducido, baseIntroducida, area, perimetro;
-
-
-
-				Rectangulo rectangulo1 = new Rectangulo();
-
-	
+				double altoIntroducido, baseIntroducido, area, perimetro;
 
 				Scanner sc2 = new Scanner(System.in);
 
@@ -96,31 +52,37 @@ public class OperacionesGeometricas {
 
 				altoIntroducido = sc2.nextDouble();
 
-
-
 				System.out.print("\nIntroduce la base del rectangulo: ");
 
-				baseIntroducida = sc2.nextDouble();
+				baseIntroducido = sc2.nextDouble();
 
-
-
-				rectangulo1.setAlto(altoIntroducido);
-
-				rectangulo1.setBase(baseIntroducida);
-
-
+				Rectangulo rectangulo1 = new Rectangulo(altoIntroducido, baseIntroducido);
 
 				area = rectangulo1.area();
 
 				perimetro = rectangulo1.perimetro();
-
-
 
 				System.out.println("\nEl area del rectangulo es: " + area + "m2");
 
 				System.out.println("\nEl perimetro del rectangulo es: " + perimetro + "m");
 
 			}
+
+			else {
+
+				System.out.print("\nCaracter no valido");
+
+			}
+			
+			System.out.print("\nIngresa la figura geometrica Circulo (C) o Rectangulo (R) o Terminar el programa (T): ");
+
+			respuesta = sc.nextLine();
+ 
+			respuestaChar = respuesta.charAt(0);
+
+		}
+
+		System.out.print("\nFin\n");
 
 	}
 
